a06763b20a8ba6786b3e51fb7863765f7a1f69bb
einfachmax <centsgames@gmail.com>
2021-10-01 17:47:12 +0200

    Update items.sql

--- items.sql
+++ items.sql
@@ -1,35 +1,3 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Datenbank: `es_extended`
---
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `items`
---
-
-CREATE TABLE `items` (
-  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `label` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `weight` int(11) NOT NULL DEFAULT 1,
-  `rare` tinyint(4) NOT NULL DEFAULT 0,
-  `can_remove` tinyint(4) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Daten für Tabelle `items`
---
-
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
 
 ('cooking_pot', 'Kochtopf', 1, 0, 1),
